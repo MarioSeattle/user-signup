b334f3f686788b6487b471a333d78e63a539ba91
MarioSeattle <marioseattle@live.com>
2017-07-07 14:11:11 -0700

    Fixed validation error for password

--- main.py
+++ main.py
@@ -17,20 +17,17 @@ def index():
     password_error = ""
     verify_error = ""
     #if username and not username.isspace():
-    if " " in username:
-        username_error = "Username cannot contain any spaces."
-    #    username_error = "Username cannot be blank."
-    elif len(username) < 3 or len(username) > 20:
-        username_error = "Username must be between 3 and 20 characters."
-    #if username and not username.isspace():
-    if len(password) < 3 or len(password) > 20:
-        password_error = "Password must be between 3 and 20 characters."
-    elif " " in password:
-        password_error = "Password cannot contain any spaces."
-    #if verify and not verify.isspace():
-    if verify != password:
+    
+    if len(username) < 3 or len(username) > 20 or username == "":
+        username_error = "Username must be between 3 and 20 characters and cannot contain any spaces."
+
+    if len(password) < 3 or len(password) > 20 or password == "":
+        password_error = "Password must be between 3 and 20 characters and cannot contain any spaces."
+    
+    if len(password) is not len(verify) or password != verify:
         verify_error= "Password does not match!"
-    elif not username_error and not password_error and not verify_error: 
+
+    if not username_error and not password_error and not verify_error: 
         return redirect('/welcome?username={0}'.format(username))
     else:
         return render_template('register.html', username=username, username_error=username_error, password_error=password_error, verify_error=verify_error)
