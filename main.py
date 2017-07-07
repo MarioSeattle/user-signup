9fe1f78a0b3165c29f50d230dbe678d64f150f4d
MarioSeattle <marioseattle@live.com>
2017-07-07 16:55:16 -0700

    Added email to signup

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ app.config['DEBUG'] = True
 def index():
     if request.method == "GET":
         return render_template('register.html')
+
     #Input from the form (POST)
+    email = request.form['email']
     username = request.form['username']
     password = request.form['password']
     verify = request.form['verify']
+    email_error = ""
     username_error = ""
     password_error = ""
     verify_error = ""
-    #if username and not username.isspace():
-    
+
+    if len(email) < 3 or len(email) > 20 or email == "":
+        email_error = "email must be between 3 and 20 characters and cannot contain any spaces."
+
+    if "@" not in email or "." not in email:
+        email_error = "Please enter a valid email, @ and '.' required"
+
     if len(username) < 3 or len(username) > 20 or username == "":
         username_error = "Username must be between 3 and 20 characters and cannot contain any spaces."
 
@@ -27,10 +35,12 @@ def index():
     if len(password) is not len(verify) or password != verify:
         verify_error= "Password does not match!"
 
-    if not username_error and not password_error and not verify_error: 
+    if not email_error and not username_error and not password_error and not verify_error: 
         return redirect('/welcome?username={0}'.format(username))
     else:
-        return render_template('register.html', username=username, username_error=username_error, password_error=password_error, verify_error=verify_error)
+        return render_template('register.html', email=email, email_error=email_error, username=username, 
+        username_error=username_error, password_error=password_error, verify_error=verify_error)
+
 #this handler will render the welcome user 
 @app.route('/welcome')
 def welcome():
